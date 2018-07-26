6fcf46f60f1dbb2702ccb5c5abe88d50aa4a77c0
Ana Lisnic <ana.lisnnic@gmail.com>
2018-07-26 21:29:41 +0300

    added arrow component

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Icon from '../icon';
+import Arrow from '../arrow';
 
 export default class LibraryCourse extends Component {
     render() {
         return (
             <div className="library-course">
-                <label className="library-coure__title">Problem Solving</label>
-                {Icon('fas fa-check', 'library-course__icon')}
-
+                <div className="library-course__title-icon">
+                    <label className="library-course__title">Problem Solving</label>
+                    { Icon('fas fa-check', 'library-course__icon') }
+                </div>
+                    <Arrow className="library-course__arrow"/>
+               
                 <div className="library-course__description">
                  <label>Course Description</label>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras auctor convallis ante, non elementum odio tincidunt a. Cras eu orci ullamcorper, vestibulum lorem nec, pulvinar velit. Cras non dolor at purus dignissim vulputate.</p>
