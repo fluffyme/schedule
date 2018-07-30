38eb80f4577b56d6bcae19f0015d99f1fb513e7b
Ana Lisnic <ana.lisnnic@gmail.com>
2018-07-30 20:36:38 +0300

    created toggleCourse action creator and reducer and connected libraryCourse component to redux to call that within action.js.

--- src/components/schedule/scheduleCourse.js
+++ src/components/schedule/scheduleCourse.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
-
-export default class ScheduleCourse extends Component {
+class ScheduleCourse extends Component {
     render() {
         return (
             <div className="schedule-course">
@@ -10,3 +9,5 @@ export default class ScheduleCourse extends Component {
         );
     }
 }
+
+export default ScheduleCourse;
