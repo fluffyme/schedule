ab4925f09a947fa86c25ed9764d220873ab91860
Ana Lisnic <ana.lisnnic@gmail.com>
2018-08-01 17:34:02 +0300

    added course selected styles

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -18,25 +18,34 @@ class LibraryCourse extends Component {
 
     renderDescription = function() {
         if(!this.state.status) {
-        
-            <div className="library-course__description">
-                <label>Course Description</label>
-                <p>
-                { this.props.description }
-                </p>
-            </div>   
-        }     
+
+
+            return(
+                <div className="library-course__description">
+                    <label>Course Description</label>
+                    <p>
+                    { this.props.description }
+                    </p>
+                </div> 
+            )  
+        } 
         
     }.bind(this);
 
     handleCallback = function(status) {
-        this.setState({ status })
+    
+        if(!status) {
+          document.getElementById('library-course').classList.add('library-course-selected');
+        }else{
+            document.getElementById('library-course').classList.remove('library-course-selected');
+        }
+        this.setState({status})
     }.bind(this);
 
     render() {
 
         return (
-            <div className="library-course">
+            <div id="library-course" className="library-course">
                 <div className="library-course__title-icon">
                     <label className="library-course__title">{ this.props.title }</label>
                     { Icon('fas fa-check', 'library-course__icon') }
